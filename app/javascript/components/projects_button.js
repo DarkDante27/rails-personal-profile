ab4b64336ad85d9bbce2e0c9376c279b7418bb78
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-06-19 22:52:33 +0900

    Updated the link on the nav bar and the main header to scroll down to the projects div in the same landing page

--- app/javascript/components/projects_button.js
+++ app/javascript/components/projects_button.js
@@ -1,12 +1,18 @@
 const loadMoreProjects = () => {
-  const moreButton = document.querySelector('#more-button-img')
+  const moreButton = document.querySelectorAll('.more-button, #more-button')
+  console.log(moreButton)
   if (moreButton) {
-    moreButton.addEventListener('click', function (e) {
-      e.preventDefault();
+    moreButton.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        console.log("forEach worked");
+        e.preventDefault();
       const projects = document.querySelector('#projects');
       projects.scrollIntoView({ behavior: 'smooth' });
     })
-  }
+  })
+}
 }
 
 export { loadMoreProjects  };
+
+
