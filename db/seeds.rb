92f67cb26963869f9abdeb908074231f0d3bd0aa
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-06-20 15:21:10 +0900

    Cards added! Now the site have cards for every project, more style will be added in the future!. - Updated the seed file again to change the urls of the images that are now in a smaller size to make them fit in the grid. -Added some basis css to the cards. -Updated the controller to pass params for the projects to the home page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,16 +24,16 @@ end
 puts 'Adding urls to the projects....'
 
 dream = Project.where(title: 'Dream-OM')
-dream.first.photos = ['https://res.cloudinary.com/dduochwyb/image/upload/v1561003947/My%20Website%20photos/dream-om_1_zwpmrw.png', 'https://res.cloudinary.com/dduochwyb/image/upload/v1561003945/My%20Website%20photos/dream-om_2_vxlftd.png', 'https://res.cloudinary.com/dduochwyb/image/upload/v1561003945/My%20Website%20photos/dream-om_3_x4yka5.png']
+dream.first.photos = ['https://res.cloudinary.com/dduochwyb/image/upload/c_mpad,h_480,q_100,w_640/v1561003947/My%20Website%20photos/dream-om_1_zwpmrw.png', 'ttps://res.cloudinary.com/dduochwyb/image/upload/c_mpad,h_480,q_100,w_640/v1561003945/My%20Website%20photos/dream-om_2_vxlftd.png', 'https://res.cloudinary.com/dduochwyb/image/upload/c_mpad,h_480,q_100,w_640/v1561003945/My%20Website%20photos/dream-om_3_x4yka5.png']
 dream.first.save
 obbis = Project.where(title: 'Obbis')
-obbis.first.photos = ['https://res.cloudinary.com/dduochwyb/image/upload/v1561003946/My%20Website%20photos/obbis_v7xruy.png']
+obbis.first.photos = ['https://res.cloudinary.com/dduochwyb/image/upload/c_mpad,h_480,q_100,w_640/v1561003946/My%20Website%20photos/obbis_v7xruy.png']
 obbis.first.save
 cocktail = Project.where(title: 'Cocktail Nebula')
-cocktail.first.photos = ['https://res.cloudinary.com/dduochwyb/image/upload/v1561003945/My%20Website%20photos/cocktail_fkgmi2.png']
+cocktail.first.photos = ['https://res.cloudinary.com/dduochwyb/image/upload/c_mpad,h_480,q_100,w_640/v1561003945/My%20Website%20photos/cocktail_fkgmi2.png']
 cocktail.first.save
 profile = Project.where(title: 'Profile page')
-profile.first.photos = ['https://res.cloudinary.com/dduochwyb/image/upload/v1561003945/My%20Website%20photos/html_profile_wxzulh.png']
+profile.first.photos = ['https://res.cloudinary.com/dduochwyb/image/upload/c_mpad,h_480,w_640/v1561003945/My%20Website%20photos/html_profile_wxzulh.png']
 profile.first.save
 
 puts 'Finished!'
