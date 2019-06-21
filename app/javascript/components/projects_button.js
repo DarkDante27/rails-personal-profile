8cfe7ab0658a77efe66d6b4982c6df36e49dafc1
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-06-21 16:41:01 +0900

    centered the text on the card.

--- app/javascript/components/projects_button.js
+++ app/javascript/components/projects_button.js
@@ -1,10 +1,8 @@
 const loadMoreProjects = () => {
   const moreButton = document.querySelectorAll('.more-button, #more-button')
-  console.log(moreButton)
   if (moreButton) {
     moreButton.forEach((button) => {
       button.addEventListener('click', (e) => {
-        console.log("forEach worked");
         e.preventDefault();
       const projects = document.querySelector('#projects');
       projects.scrollIntoView({ behavior: 'smooth' });
