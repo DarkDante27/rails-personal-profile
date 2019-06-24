6b290e307de72b28da7fd2f54515d525f2e95ba7
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-06-24 17:40:11 +0900

    Added Reveal -Added Reveal npm package -Added reveal effect script on the social links and projects.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,8 @@
 import "bootstrap";
-import { loadMoreProjects, goToContact } from "../components/projects_button";
-import { hoverGradient } from "../components/hover_gradient";
+import { loadMoreProjects, goToContact } from '../components/projects_button';
+import hoverGradient from '../components/hover_gradient';
 
 loadMoreProjects();
 goToContact();
 // hoverGradient();
+
